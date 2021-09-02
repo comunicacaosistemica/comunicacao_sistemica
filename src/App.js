b3101f2e2c8261b1de907ff2eb7ad61157471ff5
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2021-09-02 04:40:58 -0300

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import udemy from './assets/Camada 3.png'
+import notebook from './assets/clay-MBP.png'
+import effect from './assets/Camada 1.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='flexInitial' >
+        <header className='top'>
+          <nav>
+          <ul>
+            <li><a href='#'>Sobre</a></li>
+            <li><a href='#'>Ministrantes</a></li>
+            <li><a href='#'>Pacotes</a></li>
+            <button>Inscreva-se</button>
+          </ul>
+          </nav>
+          <section className='sectionHeader'>
+            <div className='title'>
+            <h1>Junte-se a mais de 1.567 alunos que já comprovam a qualidade dos nossos cursos</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id pretium enim, eget convallis tellus. Mauris consequat dapibus.</p>
+            <button>Quero Começar Agora</button>
+            </div>
+            <div className='udemy'>
+              <p>Inscreva-se em:</p>
+              <img src={udemy} alt='Udemy'/>
+            </div>
+          </section>
+        </header>
+        <section className='effectVideo'>
+          <img src={notebook} alt='notebook' className='notebook'/>
+          <iframe width="405" height="240" src="https://www.youtube.com/embed/FDNNHh7TRN0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className='iframeyt'></iframe>
+          <img src={effect} alt='efeito' className='efeito'/>
+        </section>
+      </div>
     </div>
   );
 }
